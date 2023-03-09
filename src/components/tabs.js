df4b2af4421bb43096b234457a726c06e0d3be4b
SoftEngMatthew <mateus.fonto@gmail.com>
2023-03-09 11:24:01 -0300

    transfer build folder from master to gh-pages

--- src/components/tabs.js
+++
@@ -1,8 +0,0 @@
-let tabs = document.querySelectorAll(".tab");
-let indicator = document.querySelector(".indicator");
-let panels = document.querySelectorAll(".tab-panel");
-
-indicator.style.width =
-  tabs[0].getBoundingClientRect().with -
-  tabs[0].parentElement.getBoundingClientRect().width +
-  "px";
